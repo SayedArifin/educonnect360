d191d293479877c770470ddb8cc074d80ed4143c
Md Sayed Arifin <zinnatsaverdhaka@gmail.com>
2023-12-09 16:31:19 +0600

    deploy bug fixed

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,20 +1,13 @@
 "use client"
 
 import { Button } from "@nextui-org/button";
-
 import { useFormStatus } from "react-dom";
-
-
 interface SubmitButtonProps {
     text: string;
 }
 
 const SubmitButton: React.FC<SubmitButtonProps> = ({ text }) => {
     const { pending } = useFormStatus();
-
-
-
-
     return <div><Button isLoading={pending} type="submit" variant="ghost" color="danger" className="w-fit">{text}</Button></div>;
 };
 
