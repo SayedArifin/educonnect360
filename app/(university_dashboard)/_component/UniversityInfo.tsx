38b762d67e9d4fac175e1684990db941e5a3a778
Md Sayed Arifin <zinnatsaverdhaka@gmail.com>
2023-12-13 19:30:16 +0600

    see

--- app/(university_dashboard)/_component/UniversityInfo.tsx
+++ app/(university_dashboard)/_component/UniversityInfo.tsx
@@ -1,50 +1,192 @@
 "use client"
-
-import { Button, Card, CardBody, CardHeader, Checkbox, CheckboxGroup, Divider, Input } from "@nextui-org/react";
 import { useState } from "react";
+import {
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    Checkbox,
+    CheckboxGroup,
+    Divider,
+    Input,
+    Select,
+    SelectItem,
+    Textarea,
+} from "@nextui-org/react";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { IoIosRemoveCircle } from "react-icons/io";
+import ReactMarkdown from 'react-markdown';
+interface UniversityInfoProps { }
 
-interface UniversityInfoProps {
+interface Branch {
+    name: string;
+    address: string;
+    helpline: string;
+    faculties: string[];
 }
 
 const UniversityInfo: React.FC<UniversityInfoProps> = () => {
     const [isEditable, setIsEditable] = useState(false);
-    return <div>
-        <div className="flex justify-between ">
-            <h2 className="text-4xl font-bold dark:text-white">{isEditable ? "Edit University Information" : "View University Information"}</h2>
-            <Button color="danger" onClick={() => setIsEditable(!isEditable)}>{isEditable ? "Cancel" : "Edit"}</Button>
 
-        </div>
-        <Divider className="my-4" />
-        <h5 className="text-xl font-bold dark:text-white">Genaral Information</h5>
-        <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Year of Establishment:" color="danger" />
-        <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Vice Chancellor Name:" color="danger" />
-        <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Registrar Name:" color="danger" />
-        <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Official Website:" color="danger" />
-        <CheckboxGroup isReadOnly={!isEditable} color="danger" defaultValue={["bachelor"]} orientation="horizontal" label={" Available Degree:"}>
-            <Checkbox value={"bachelor"}>Bachelor</Checkbox>
-            <Checkbox value={"masters"}>Masters</Checkbox>
+    // General Information
+    const [yearOfEstablishment, setYearOfEstablishment] = useState("");
+    const [viceChancellorName, setViceChancellorName] = useState("");
+    const [registrarName, setRegistrarName] = useState("");
+    const [officialWebsite, setOfficialWebsite] = useState("");
+    const [availableDegrees, setAvailableDegrees] = useState<string[]>(["bachelor"]);
 
-        </CheckboxGroup>
+    // Contact Information
+    const [emailAddress, setEmailAddress] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [faxNumber, setFaxNumber] = useState("");
 
-        <h5 className="text-xl font-bold dark:text-white">Contect Information</h5>
-        <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Email Address:" color="danger" />
-        <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Telephone / Mobile Number:" color="danger" />
-        <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Fax Number:" color="danger" />
-        <h5 className="text-xl font-bold dark:text-white">Branch Information</h5>
-        <Card>
-            <CardHeader>
-                <h6 className="text-md font-bold dark:text-white">Branch Name</h6>
-            </CardHeader>
-            <CardBody >
-                <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Email Address:" color="danger" />
-                <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Telephone / Mobile Number:" color="danger" />
-                <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Fax Number:" color="danger" />
-            </CardBody>
-        </Card>
+    // Branch Information
+    const [branches, setBranches] = useState<Branch[]>([{ name: "", address: "", helpline: "", faculties: [] }]);
+    //details
+    const [markdownContent, setMarkdownContent] = useState("");
+    const addBranch = () => {
+        setBranches((prevBranches) => [...prevBranches, { name: "", address: "", helpline: "", faculties: [] }]);
+    };
 
+    const removeBranch = (index: number) => {
+        setBranches((prevBranches) => prevBranches.filter((_, i) => i !== index));
+    };
 
+    const handleBranchChange = (index: number, field: string, value: string) => {
+        setBranches((prevBranches) =>
+            prevBranches.map((branch, i) =>
+                i === index ? { ...branch, [field]: value } : branch
+            )
+        );
+    };
 
-    </div >;
+    const handleFacultyChange = (index: number, selectedFaculties: string[]) => {
+        setBranches((prevBranches) =>
+            prevBranches.map((branch, i) =>
+                i === index ? { ...branch, faculties: selectedFaculties } : branch
+            )
+        );
+    };
+
+    return (
+        <div>
+            <div className="flex justify-between ">
+                <h2 className="text-4xl font-bold dark:text-white">{isEditable ? "Edit University Information" : "View University Information"}</h2>
+                <Button color="danger" onClick={() => setIsEditable(!isEditable)}>{isEditable ? "Cancel" : "Edit"}</Button>
+            </div>
+            <Divider className="my-4" />
+            <h5 className="text-xl font-bold dark:text-white">General Information</h5>
+            <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Year of Establishment:" color="danger" value={yearOfEstablishment} onChange={(e) => setYearOfEstablishment(e.target.value)} />
+            <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Vice Chancellor Name:" color="danger" value={viceChancellorName} onChange={(e) => setViceChancellorName(e.target.value)} />
+            <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Registrar Name:" color="danger" value={registrarName} onChange={(e) => setRegistrarName(e.target.value)} />
+            <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Official Website:" color="danger" value={officialWebsite} onChange={(e) => setOfficialWebsite(e.target.value)} />
+            <CheckboxGroup isReadOnly={!isEditable} color="danger" defaultValue={availableDegrees} orientation="horizontal" label={" Available Degree:"}>
+                <Checkbox value={"bachelor"} onChange={() => setAvailableDegrees((prev) => ["bachelor"])}>Bachelor</Checkbox>
+                <Checkbox value={"masters"} onChange={() => setAvailableDegrees((prev) => ["masters"])}>Masters</Checkbox>
+            </CheckboxGroup>
+
+            <h5 className="text-xl font-bold dark:text-white">Contact Information</h5>
+            <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Email Address:" color="danger" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} />
+            <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Telephone / Mobile Number:" color="danger" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+            <Input type="text" isReadOnly={!isEditable} variant="underlined" label="Fax Number:" color="danger" value={faxNumber} onChange={(e) => setFaxNumber(e.target.value)} />
+
+            <h5 className="text-xl font-bold dark:text-white">Branch Information</h5>
+            {branches.map((branch, index) => (
+                <Card key={index} className="mb-4">
+                    <CardHeader>
+                        <div className="flex justify-between items-center w-full">
+                            <h6 className="text-lg font-bold">{`Branch ${index + 1}`}</h6>
+                            {isEditable && index > 0 && (
+                                <Button
+                                    color="warning"
+                                    variant="flat"
+                                    isIconOnly
+                                    onClick={() => removeBranch(index)}
+                                >
+                                    <IoIosRemoveCircle size={25} />
+                                </Button>
+                            )}
+                        </div>
+                    </CardHeader>
+                    <CardBody>
+                        <Input
+                            type="text"
+                            isReadOnly={!isEditable}
+                            variant="underlined"
+                            label="Branch Name:"
+                            color="danger"
+                            value={branch.name}
+                            onChange={(e) => handleBranchChange(index, "name", e.target.value)}
+                        />
+                        <Input
+                            type="text"
+                            isReadOnly={!isEditable}
+                            variant="underlined"
+                            label="Branch Address:"
+                            color="danger"
+                            value={branch.address}
+                            onChange={(e) => handleBranchChange(index, "address", e.target.value)}
+                        />
+                        <Input
+                            type="text"
+                            isReadOnly={!isEditable}
+                            variant="underlined"
+                            label="Branch Helpline:"
+                            color="danger"
+                            value={branch.helpline}
+                            onChange={(e) => handleBranchChange(index, "helpline", e.target.value)}
+                        />
+                        <Select
+                            label="Available Faculties"
+                            selectionMode="multiple"
+                            placeholder="Select faculties"
+                            variant="underlined"
+                            isDisabled={!isEditable}
+                            color="danger"
+                            value={branch.faculties || []}
+                            onSelectionChange={(selectedOptions) => {
+                                const selectedValues = Array.from(
+                                    selectedOptions || [],
+                                    (option) => option.valueOf()
+                                ).map((value) => String(value)); // Convert each value to a string
+
+                                handleFacultyChange(index, selectedValues);
+                            }}
+                        >
+                            <SelectItem value={"2"} key={1}>Faculty 1</SelectItem>
+                            <SelectItem value={"1"} key={2}>Faculty 2</SelectItem>
+                        </Select>
+                    </CardBody>
+                </Card>
+            ))}
+            {isEditable && (
+                <Button isIconOnly color="success" onClick={addBranch}>
+                    <AiFillPlusCircle size={25} />
+                </Button>
+            )}
+            <h5 className="text-xl font-bold dark:text-white">Details</h5>
+            <div className="flex flex-col gap-2">
+                <div>
+                    <Textarea
+                        placeholder="Enter Markdown Content"
+
+                        value={markdownContent}
+                        onChange={(e) => setMarkdownContent(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <Card>
+                        <CardHeader>
+                            <h6 className="text-lg font-bold">Live Preview</h6>
+                        </CardHeader>
+                        <CardBody>
+                            <ReactMarkdown>{markdownContent}</ReactMarkdown>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div></div>
+
+    )
 };
 
 export default UniversityInfo;
--- app/(university_dashboard)/university_dashboard/demo/page.tsx
+++ app/(university_dashboard)/university_dashboard/demo/page.tsx
@@ -1,41 +1,25 @@
-
 "use client"
-import { useState, useEffect } from 'react';
-import { ThemeSwitch } from "@/components/theme-switch";
+import { useState } from 'react';
+
 import Link from 'next/link';
-import { FaHome } from 'react-icons/fa';
 import { BiMenuAltLeft } from "react-icons/bi";
 import { MdPublishedWithChanges } from "react-icons/md";
-import { GetUniversityName } from '@/action/action';
-import { usePathname } from 'next/navigation';
 
 interface SidebarProps {
     children: React.ReactNode;
-    email: string;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ children, email }) => {
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const [u_name, setName] = useState("");
-    const pathname = usePathname();
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await GetUniversityName(email)
-            setName(res?.university_name || "")
-
-        }
-        fetchData();
-    }, [email])
 
     const sidebarItems = [
-        { icon: <FaHome />, text: "Dashboard", href: "/university_dashboard" },
-        { icon: <MdPublishedWithChanges />, text: "Modify University", href: "/university_dashboard/modify" },
-
-
+        { icon: <BiMenuAltLeft size={25} />, text: "Dashboard", href: "/dashboard" },
+        { icon: <MdPublishedWithChanges />, text: "Modify", href: "/dashboard/modify" },
+        // Add more sidebar items as needed
     ];
 
     return (
@@ -44,32 +28,32 @@ const Sidebar: React.FC<SidebarProps> = ({ children, email }) => {
                 <div className="px-3 py-3 lg:px-5 lg:pl-3">
                     <div className="flex items-center justify-between">
                         <div className="flex items-center justify-start rtl:justify-end">
-                            <button onClick={toggleSidebar} data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                            <button onClick={toggleSidebar} type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                                 <BiMenuAltLeft size={25} className='mr-2' />
                             </button>
                             <p className="font-bold text-inherit text-2xl">
-                                EduConnect<span className="text-pink-500">360</span>°<span className="text-sm">{u_name}</span>
+                                app Name
                             </p>
                         </div>
                         <div className="flex items-center">
                             <div className="flex items-center ms-3">
                                 <div>
-                                    <ThemeSwitch />
+                                    Contant
                                 </div>
-
                             </div>
-                        </div></div></div>
+                        </div>
+                    </div>
+                </div>
             </nav>
 
-            <aside id="logo-sidebar" className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700`} aria-label="Sidebar">
+            <aside className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700`} aria-label="Sidebar">
                 <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
                     <ul className="space-y-2 font-medium">
                         {sidebarItems.map((item, index) => (
                             <li key={index}>
-                                <Link href={item.href} className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${item.href === pathname && "bg-gray-200 dark:bg-gray-700"}`}>
+                                <Link href={item.href} className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group`}>
                                     {item.icon}
                                     <span className="ms-3 flex-1 whitespace-nowrap">{item.text}</span>
-
                                 </Link>
                             </li>
                         ))}
